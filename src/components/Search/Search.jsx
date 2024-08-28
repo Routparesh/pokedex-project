e56bf8a4200a39f7b17d04e870d806e771e6ad52
Paresh Kumar Rout <routpareshkumar737@gmail.com>
2024-08-29 00:38:40 +0530

    implement useDebounce hook

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import useDebounce from '../../hooks/useDebounce';
 import './Search.css';
 
 const Search = ({ updateSearchterm }) => {
+	const DebounceCallBack = useDebounce((e) => updateSearchterm(e.target.value));
 	return (
 		<div className="search-wrapper">
 			<input
-				onChange={(e) => updateSearchterm(e.target.value)}
+				onChange={DebounceCallBack}
 				id="pokemon-name-search"
 				type="text"
 				placeholder="pokemon name"
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,11 @@
+const useDebounce = (cb, delay = 2000) => {
+	let timerId;
+	return (...args) => {
+		clearInterval(timerId);
+		timerId = setTimeout(() => {
+			cb(...args);
+		}, delay);
+	};
+};
+
+export default useDebounce;
